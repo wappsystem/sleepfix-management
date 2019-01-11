07f22df6981702bdc4479ee4e53f35b0b50bd351
Gunnar Unger <gunnar.unger@gmail.com>
2019-01-12 10:06:55 +1100

    update

--- modules/tasks/task-data.js
+++ modules/tasks/task-data.js
@@ -8,3 +8,10 @@ m.before_submit=function(record,dbv){
    return r;
 };
 //-------------------------------------
+m.set_req_export=function(i1,i2){
+    var sql="with participant as (select Participant_uid=UID,RowNum=row_number() over (order by PUID) from [TABLE-20011505])";
+    sql+=",tb as (select PUID,Information,DateTime from [TABLE-"+m.db_pid+"-@S1] ) ";
+    sql+="select Participant_uid,Information,DateTime from participant left join tb on PUID=Participant_uid where RowNum between @I1 and @I2";
+	m.req={cmd:'read',qid:m.qid,sql:sql,i1:i1,i2:i2};
+}
+//-------------------------------------
